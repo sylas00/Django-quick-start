81f9dabb4fd6e70c2a836165e0688f19fd6e6e3a
xu.jin <xyjy247@gmail.com>
2022-04-15 14:25:34 +0800

    review

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
