94e06ccb00226b7506cad1a650ff867bf0247be0
xu.jin <xyjy247@gmail.com>
2022-04-15 19:58:49 +0800

    chore:封装response

---
+++ libs/response.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
+
+from libs.exception_enum import StatusCodeEnum
+
+"""
+基于DRF -- Response 的自定义响应
+"""
+
+
+class RootResponse(Response):
+    pass
+
+
+class APIResponse(Response):
+    def __init__(self, data: dict):
+        """
+        常用API响应
+        :param data: 响应数据
+        """
+        data = {"code": StatusCodeEnum.SUCCESS.code, "msg": StatusCodeEnum.SUCCESS.msg, "data": data}
+        super().__init__(status=status.HTTP_200_OK, data=data, )
