4654e4dc175f6b2c64f7c4a0a18fe15d46e1058e
xu.jin <xyjy247@gmail.com>
2022-04-18 14:51:04 +0800

    add log config

--- configs/settings/dev_settings.py
+++ configs/settings/dev_settings.py
@@ -120,3 +120,56 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+# 日志
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'verbose',
+        },
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django.dbg': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['file', 'mail_admins'],
+            # 'handlers': ['console'],
+
+            'level': 'WARNING',
+            'propagate': False,
+        },
+    }
+}
